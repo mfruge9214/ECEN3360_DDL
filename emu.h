fc2b672a55d0227c24787dd31780f222f32c61d1
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2019-02-02 12:10:02 -0700

    Lab 2 work

---
+++ emu.h
@@ -0,0 +1,16 @@
+// Include Files
+#include "em_emu.h"
+#include <stdbool.h>
+#include <em_core.h>
+
+
+
+
+// Global Variables
+
+
+
+//Function Prototypes
+void blockSleepMode(uint8_t);
+void unblockSleepMode(uint8_t);
+void sleep(void);
